4c5b9c919bf0fb13ede9c7af05ba6c6473738dbb
Pavan Kumar Sampara <pavan.sampara@gmail.com>
2022-07-28 05:37:31 +0530

    Update MangaSee.kt

--- multisrc/overrides/nepnep/mangasee/src/MangaSee.kt
+++ multisrc/overrides/nepnep/mangasee/src/MangaSee.kt
@@ -1,7 +1,6 @@
 package eu.kanade.tachiyomi.extension.en.mangasee
 
 import eu.kanade.tachiyomi.multisrc.nepnep.NepNep
-import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
